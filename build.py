8ccd88c4e9486190e922d41344d0eaf70230ba59
gowthamdongari <gowthamdnr@gmail.com>
2017-06-25 13:40:40 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,19 @@
-import operator
+from operator import itemgetter
 
 
 def solution_asc(dic):
     '''
     Enter your code here
     '''
+    dic1 = list(dic.items())
+    lst_asc = sorted(dic1,key=itemgetter(0))
+    return lst_asc
 
 
 def solution_desc(dic):
     '''
     Enter your code here
     '''
-
+    dic1 = list(dic.items())
+    lst_dsc = sorted(dic1,key=itemgetter(0),reverse=True)
+    return lst_dsc
